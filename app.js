aca303a1e1e28d0e4474609e00e86802674076f7
Ricardo Costa Savluchinske <119826203+ricardosavlu@users.noreply.github.com>
2023-06-16 12:41:34 +0000

    chore: convert app to use es6 modules

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var app = express();
+// import { teste1 } from "./teste1";
+// import { teste2 } from "./teste2";
+// import { teste3 } from "./teste3";
+// import { teste4 } from "./teste4";
+// import { teste5 } from "./teste5";
+import express from 'express';
 
-var teste1 = require("./teste1");
-var teste2 = require("./teste2");
-var teste3 = require("./teste3");
-var teste4 = require("./teste4");
-var teste5 = require("./teste5");
+const PORT = 3000;
+
+const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.send(`get user/ </br>
   get users/ </br>
   post users/ </br>
@@ -19,15 +21,14 @@ app.get('/', function(req, res){
   `);
 });
 
-app.get("/user", teste1.getUser);
-app.get("/users", teste1.getUsers);
-app.post("/users", teste2)
-app.delete("/users", teste3)
-app.put("/users", teste4)
-app.get("/users/access", teste5);
+// app.get("/user", teste1.getUser);
+// app.get("/users", teste1.getUsers);
+// app.post("/users", teste2)
+// app.delete("/users", teste3)
+// app.put("/users", teste4)
+// app.get("/users/access", teste5);
 
 
-const port  = 3000;
-app.listen(port, function(){
-  console.log('Express server listening on port ' + port);
+app.listen(PORT, function () {
+  console.log('Express server listening on port ' + PORT);
 });
