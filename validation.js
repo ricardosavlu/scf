a3ebf5237414b98bf0732481e5a4d2165e5d025a
Ricardo Costa Savluchinske <119826203+ricardosavlu@users.noreply.github.com>
2023-06-16 14:38:10 +0000

    feat: add validation for valid role types

--- validation.js
+++ validation.js
@@ -1,22 +1,31 @@
-const ID_IS_REQUIRED = 'id is required'
-const NAME_IS_REQUIRED = 'name is required'
-const JOB_IS_REQUIRED = 'job is required'
-const ROLE_IS_REQUIRED = 'role is required'
+import { Role } from "./fakeData.js"
+
+const ERR_PREFIX = 'validation failed: '
+
+const ID_IS_REQUIRED = ERR_PREFIX + 'id is required'
+const NAME_IS_REQUIRED = ERR_PREFIX + 'name is required'
+const JOB_IS_REQUIRED = ERR_PREFIX + 'job is required'
+const INVALID_ROLE = ERR_PREFIX + 'role is invalid'
 
 export function validateUserCreateInput(input) {
-    if(!input.name) return NAME_IS_REQUIRED
-    if(!input.job) return JOB_IS_REQUIRED
-    if(!input.role) return ROLE_IS_REQUIRED
+    if (!input.name) return NAME_IS_REQUIRED
+    if (!input.job) return JOB_IS_REQUIRED
+    if (!input.role && !roleIsValid(input.role)) return INVALID_ROLE
 }
 
 export function validateDeleteUserInput(input) {
-    if(!input.name) return NAME_IS_REQUIRED
+    if (!input.name) return NAME_IS_REQUIRED
 }
 
 export function validateUpdateUserInput(input) {
-    if(!input.id) return ID_IS_REQUIRED
+    if (!input.id) return ID_IS_REQUIRED
+    if (!input.role && !roleIsValid(input.role)) return INVALID_ROLE
 }
 
 export function validateGetReadCountInput(input) {
-    if(!input.name) return NAME_IS_REQUIRED
+    if (!input.name) return NAME_IS_REQUIRED
+}
+
+function roleIsValid(role) {
+    return Object.values(Role).includes(role)
 }
