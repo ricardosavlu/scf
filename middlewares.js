965b887ce49faeccee35280573c5d5a71996e5e7
Ricardo Costa Savluchinske <119826203+ricardosavlu@users.noreply.github.com>
2023-06-16 14:37:56 +0000

    fix: null checks in loggedIn middleware

--- middlewares.js
+++ middlewares.js
@@ -1,10 +1,10 @@
 import { Role, fakeUsers } from "./fakeData.js"
 
 export const loggedIn = async (req, res, next) => {
-    const userId = req.headers['authorization'].split('Basic ')[1]
+    const userId = req.headers['authorization']?.split('Basic ')?.[1]
     if (!userId) return res.sendStatus(401)
 
-    const user = await fakeUsers.findById(userId)
+    const user = await fakeUsers.findById(Number(userId))
     if (!user) return res.sendStatus(401)
 
     req.user = user // Add user object to the request context
@@ -25,7 +25,7 @@ export const minRole = (role) => (req, res, next) => {
     const requiredValue = ROLE_VALUES[role]
     const userValue = ROLE_VALUES[req.user.role]
 
-    if (userValue < requiredValue) return restart.sendStatus(403)
+    if (userValue < requiredValue) return res.sendStatus(403)
 
     next()
 }
