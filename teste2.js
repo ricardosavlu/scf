06db26af23fbb24da98606f1868d192867e623ef
Ricardo Costa Savluchinske <119826203+ricardosavlu@users.noreply.github.com>
2023-06-16 13:38:11 +0000

    fix(Test2): add validation for unique names

--- teste2.js
+++ teste2.js
@@ -11,6 +11,14 @@ export const createUser = async (req, res) => {
 
     const { name, job } = req.body;
 
+    const nameIsTaken = await fakeUsers.findByName(name)
+
+    if (nameIsTaken) {
+        // We need users to have unique names...
+        res.status(409)
+        res.send(`name "${name}" is already in use`)
+    }
+
     const newUser = await fakeUsers.createUser({ name, job })
 
     res.send(newUser);
